4d1bbdd49462355f7abc25170b9ced2b545e1e07
Matthew Klein <mtt.l.kln@gmail.com>
2020-04-28 22:01:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  data.reduce({}) do |memo, (key, value)|
+  p memo # First block parameter
+  p key # Second block parameter
+  p value # Second block parameter
+  memo # Return value for the block, becomes the memo in the next go-round
+end
+
+
+
+  
 end
